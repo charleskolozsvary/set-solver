e5c04aa31cb1c97371a6553103520a69fa04f081
charlesk26 <55702421+charlesk26@users.noreply.github.com>
2024-01-31 13:31:32 -0500

    Add files via upload

---
+++ find.py
@@ -0,0 +1,133 @@
+'''
+Given basic card information, find the sets
+'''
+import extract
+import identify
+import cv2 as cv
+import itertools
+import streamlit as st
+import numpy as np
+import time
+
+DISPLAY_SET_COLOR = (25, 75, 225)
+LABEL_COLOR = (50, 120, 225)
+CONTOUR_WIDTH_PROP = 0.0003
+TEXT_SCALING_PROP = 0.0000135
+MIN_CONTOUR_DRAW_WIDTH = 12
+MIN_TEXT_SIZE = 2.5
+MAX_DISPLAY_TIME = 20
+
+FEATURES = [['R', 'G', 'P'], ['RH', 'OV', 'SQ'], ['1', '2', '3'], ['F', 'E', 'D']]
+
+def getComplementCard(c1, c2):
+    complement = []
+    card1, card2 = c1.split(':'), c2.split(':')
+    for i in range(4):
+        if card1[i] == card2[i]:
+            complement.append(card1[i])
+        else:
+            options = list(FEATURES[i])
+            options.remove(card1[i])
+            options.remove(card2[i])
+            complement.append(options[0])
+    return ':'.join([complement[0], complement[1], complement[2], complement[3]])
+    
+def getSets(cards): #cards is dict with key of strings representing cards and the value as the index which the card corresponds to in the
+    sets = []
+    for pair in itertools.combinations(cards.keys(), 2):
+        comp = getComplementCard(pair[0], pair[1])
+        if comp in cards.keys():
+            set = frozenset([cards[pair[0]], cards[pair[1]], cards[comp]])
+            if set not in sets:
+                sets.append(set)
+    return sets
+    
+    
+def displaySets(originalImage, sets, cardContours, cardCenters, labels):
+    labeledImg = originalImage.copy()
+    s1 = time.time()
+    drawing_widths = []
+    for i, label in enumerate(labels):
+        cnt = cardContours[i]
+        area = cv.contourArea(cnt)
+        drawing_widths.append(max(MIN_CONTOUR_DRAW_WIDTH, int(CONTOUR_WIDTH_PROP * area)))
+        text_size = max(MIN_TEXT_SIZE, area * TEXT_SCALING_PROP)
+        cv.putText(labeledImg, label, cardCenters[i], cv.FONT_HERSHEY_SIMPLEX, text_size, LABEL_COLOR, int(text_size * 3), 2)
+    total_time = time.time() - s1
+    solutions = []
+    for set in sets:
+        assert total_time < MAX_DISPLAY_TIME, 'It has taken longer than '+str(MAX_DISPLAY_TIME)+' seconds to display the sets...'
+        start = time.time()
+        solution = originalImage.copy()
+        for index in set:
+            cv.drawContours(solution, [cardContours[index]], 0, DISPLAY_SET_COLOR, drawing_widths[index])
+        solutions.append(solution)
+        total_time += time.time() - start
+    return solutions, labeledImg
+    
+def dist(p1, p2):
+    return np.sqrt((p1[0]-p2[0]) ** 2 + (p1[1]-p2[1]) ** 2)
+
+def remove_insert(arr, old, new, idx):
+    if type(old) == type(np.array([], dtype = 'int32')):
+        for i, cnt in enumerate(arr):
+            if (cnt == old).all():
+                del arr[idx]
+                arr.insert(idx, new.copy())
+                break
+        else:
+            st.write('old contour was not present')
+    else:
+        arr.remove(old)
+        arr.insert(idx, new)
+    
+def removeDuplicateCards(contours, positions, labels, center_of_img, imgs, orig_img):
+    cards = {}
+    unique_contours, unique_positions = [], []
+    card_idx = 0
+    for i, label in enumerate(labels):
+        key = ':'.join([label[0][0], label[1][:2], label[2][0], label[3][0]]).upper()
+        if key in cards.keys():
+            st.write('duplicate handeled...')
+            if dist(positions[i], center_of_img) < dist(positions[cards[key][1]], center_of_img):
+                remove_insert(unique_contours, contours[cards[key][1]], contours[i], cards[key][0])
+                remove_insert(unique_positions, positions[cards[key][1]], positions[i], cards[key][0])
+            continue
+        else:
+            cards[key] = (card_idx, i)
+            unique_contours.append(contours[i].copy())
+            unique_positions.append(positions[i])
+            card_idx += 1
+    final_cards = {}
+    for key in cards:
+        final_cards[key] = cards[key][0]
+    return final_cards, unique_contours, unique_positions
+    
+    
+def find_sets(orig_img): #img is in RGB
+    s1 = time.time()
+    cardImages, cardContours, cardCenters = extract.getCardImagesAndTheirContours(orig_img)
+    e1 = time.time()
+    t1 = e1 - s1
+    st.write('time taken for extract: ', t1)
+    s2 = time.time()
+    cardLabels = identify.getCardLabels(cardImages)
+    e2 = time.time()
+    t2 = e2 - s2
+    st.write('time taken for identify: ', t2)
+    s3 = time.time()
+    cards, unique_contours, unique_positions = removeDuplicateCards(cardContours, cardCenters, cardLabels, (orig_img.shape[0] // 2, orig_img.shape[1] // 2), cardImages, orig_img)
+    e3 = time.time()
+    t3 = e3 - s3
+    st.write('time taken for remove duplicate cards: ', t3)
+    s4 = time.time()
+    sets = getSets(cards)
+    e4 = time.time()
+    t4 = e4 - s4
+    st.write('time taken for getSets: ', t4)
+    s5 = time.time()
+    solutions, labels = displaySets(orig_img, sets, unique_contours, unique_positions, cards.keys())
+    e5 = time.time()
+    t5 = e5 - s5
+    st.write('time taken for displaySets: ', t5)
+    return solutions, labels
---
+++ setSolver.py
@@ -0,0 +1,44 @@
+import cv2 as cv
+import streamlit as st
+import numpy as np
+from PIL import Image
+from PIL import ImageOps
+import find
+import time
+
+def solve(img):
+    return find.find_sets(img)
+
+def main_loop():
+    ess = time.time()
+    st.title("Set Solver")
+    s1 = time.time()
+    upload = st.file_uploader("Upload an image", type=['jpg', 'png', 'jpeg'])
+    img = None
+    if upload is not None:
+        file_bytes = np.asarray(bytearray(upload.read()), dtype=np.uint8)
+        opencv_image = cv.imdecode(file_bytes, 1)
+        img = cv.cvtColor(opencv_image, cv.COLOR_BGR2RGB)
+    else:
+        return None
+    e1 = time.time()
+    t1 = e1 - s1
+    st.write('time for uploading image: ', t1)
+    
+    start = time.time()
+    solutions, labelings = solve(img)
+    end = time.time()
+    st.header("Total Execution Time: "+str(end - start))
+    st.header("Card labels")
+    st.image(labelings)
+    if len(solutions) == 0:
+        st.header("There are no sets")
+    else:
+        st.header("There are "+str(len(solutions))+" sets" if len(solutions) >= 2 else "There is 1 set")
+        for solution in solutions:
+            st.image(solution)
+    enn = time.time()
+    st.header('total time (for real?): '+str(enn-ess))
+        
+if __name__ == '__main__':
+    main_loop()
